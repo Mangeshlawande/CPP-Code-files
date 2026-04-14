8fd7c869f6f87e91d53f7d7d7abb6c99a95f6237
Mangeshlawande <mangeshlawande223@gmail.com>
2026-04-14 21:20:12 +0530

    another types of numops

--- Rythmflow/conversions/numops.cpp
+++ Rythmflow/conversions/numops.cpp
@@ -78,6 +78,25 @@ int fib(int num)
     return fib(num - 1) + fib(num - 2);
 }
 
+long long int fact(long long n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+
+long int factorial(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    };
+    return fact;
+}
+
 int main()
 {
     int n = 6;
@@ -115,6 +134,20 @@ int main()
     cin >> s;
 
     cout << "The sum = " << sumOfDig(s) << endl;
+
+    int n;
+    cout << "Enter a number to calculate factorial : ";
+    cin >> n;
+
+    cout << "Factorial of number " << n << " is " << factorial(n) << endl;
+
+    cout << "Enter a range to calculate factorial : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+
+        cout << "The factorial of number " << i << " is " << fact(i) << endl;
+    }
 
     return 0;
 }
